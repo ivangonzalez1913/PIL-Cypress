411f99dea50daa5de19110610445e177049d16bd
Ivan Gonzalez <ivangonzalezscaglia1@gmail.com>
2023-09-11 20:59:52 -0300

    Test de servicio cuartetos y festivales

--- cypress/e2e/1-EDEN/edenServicios.cy.js
+++ cypress/e2e/1-EDEN/edenServicios.cy.js
@@ -13,12 +13,53 @@ describe("TEST SERVICIOS", () => {
       expect(response.status).to.eq(200);
     });
   });
-  it.only("Validar schema", () => {
+  it("Validar schema eventos", () => {
     cy.callServiceCheck(
       "GET",
       "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio",
       "eventos_schema",
       "eventos"
+    );
+  });
+
+  it("Verificar servicio de cuartetos", () => {
+    cy.request({
+      method: "GET",
+      url: "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/eventos/cuartetos",
+    }).then((response) => {
+      cy.writeFile(
+        `cypress/fixtures/autogenerado/cuartetos.json`,
+        response.body
+      );
+      expect(response.status).to.eq(200);
+    });
+  });
+  it("Validar schema cuartetos", () => {
+    cy.callServiceCheck(
+      "GET",
+      "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/eventos/cuartetos",
+      "cuartetos_schema",
+      "cuartetos"
+    );
+  });
+  it.only("Verificar servicio de festivales", () => {
+    cy.request({
+      method: "GET",
+      url: "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/eventos/festivales",
+    }).then((response) => {
+      cy.writeFile(
+        `cypress/fixtures/autogenerado/festivales.json`,
+        response.body
+      );
+      expect(response.status).to.eq(200);
+    });
+  });
+  it.only("Validar schema festivales", () => {
+    cy.callServiceCheck(
+      "GET",
+      "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/eventos/festivales",
+      "festivales_schema",
+      "festivales"
     );
   });
 });
