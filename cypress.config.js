2cda1aa2833cce84ccbf74a77d54ef6750044d8f
Ivan Gonzalez <ivangonzalezscaglia1@gmail.com>
2023-09-11 15:23:02 -0300

    Cambio en las config para uso de tags

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: { grepFilterSpecs: true, grepOmitFiltered: true },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@bahmutov/cy-grep/src/plugin')(config);
+      return config
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
-
+import "./commands";
+const registerCypressGrep = require("@bahmutov/cy-grep");
+registerCypressGrep();
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
